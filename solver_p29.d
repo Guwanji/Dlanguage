8c7b4816ca673072a31fed9c742f33da7812d357
Guwanji <147578983+Guwanji@users.noreply.github.com>
2023-11-22 06:16:28 +0700

    Add files via upload

---
+++ solver_p29.d
@@ -0,0 +1,34 @@
+module solver_p29;
+import std.stdio;
+import io;
+
+void solve_sort_indexes(int[] data){
+    //TODO: Solve the solution
+    //print!int.printArray(data)
+    int[] indices =  new int[data.length];
+    for(int i=0;i<data.length;i++)
+        indices[i]= i;
+    
+    //prepare double loop
+    for(int i=0;i<data.length;i++){
+        for(int j=0;j<data.length-1;j++){
+            if(data[j]>data[j+1]){
+                //Todo: swap data
+                swap(data[j], data[j+1]);
+                swap(indices[j],indices[j+1]);
+            }
+        }
+    }
+    print!int.printArray(data);
+
+    for(int i=0;i<data.length;i++){
+        write(indices[i]+1,"\t");
+    }
+}
+
+void swap(ref int a, ref int b){
+    int temp = 0;
+    temp = b;
+    b = a;
+    a = temp;
+}
